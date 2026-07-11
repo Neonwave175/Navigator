027091eb41aaedf88b6435d5ac46abd812deee94
Neonwave175 <bhuvan.jeyaganesan@gmail.com>
2026-07-11 12:52:10 +0530

    by Bhuvan Jeyagesnan

--- Server/Mlx.py
+++ Server/Mlx.py
@@ -1,3 +1,4 @@
+import time as t
 import warnings
 
 from mlx_vlm.generate import generate
@@ -5,10 +6,17 @@ from mlx_vlm.utils import load
 
 warnings.filterwarnings("ignore", message=".*mel filter.*")
 
+# Benchmarking
+start = t.perf_counter()
 model, processor = load("mlx-community/gemma-4-26b-a4b-it-mxfp4")
 
+# Benchmarking
+elapsed = t.perf_counter() - start
+print(f"Elapsed: {elapsed}\n")
+
+
 # This is the location of the image, edit this if needed
-image_path = "images/Linus.jpg"
+image_path = "images/Newt.jpg"
 
 messages = [
     {
@@ -25,6 +33,8 @@ messages = [
 prompt = processor.apply_chat_template(
     messages, tokenize=False, add_generation_prompt=True
 )
+
+start = t.perf_counter()
 result = generate(
     model,
     processor,
@@ -34,3 +44,5 @@ result = generate(
     temperature=0.0,
 )
 print(result.text)
+elapsed = t.perf_counter() - start
+print(f"Elapsed: {elapsed}")
