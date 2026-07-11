c94ec2ddc0fbc9b3f2b9fb9fba969566bf620522
Neonwave175 <bhuvan.jeyaganesan@gmail.com>
2026-07-11 13:35:51 +0530

    by Bhuvan Jeyagesnan

--- Server/enctest.py
+++ Server/enctest.py
@@ -2,7 +2,7 @@ import encrypt as e
 
 KEYSEED = 948729038745908237459734957230573098475092
 key = e.buildkey(KEYSEED)
-images = ["Concorde", "Fuhrer", "Jensen", "Linus", "Newt", "SteveJobs"]
+images = ["Concorde", "Jensen", "Linus", "Newt", "SteveJobs"]
 for i in images:
     e.encrypt_image(f"images/{i}.jpg", key, f"encrypted/{i}.enc")
     e.decrypt_image(f"encrypted/{i}.enc", key, f"decrypted/{i}.jpg")
